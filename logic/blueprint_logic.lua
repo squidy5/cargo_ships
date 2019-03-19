9db43bde28ea6ec649c1f380516b4f96bb6ae09f
Sven Zimmermann <szimmermann@uos.de>
2019-03-19 19:14:02 +0100

    bugfixes

--- logic/blueprint_logic.lua
+++ logic/blueprint_logic.lua
@@ -1,21 +1,23 @@
 function FixBlueprints(e)
 	local stack = game.players[e.player_index].cursor_stack
-	if not(stack and stack.valid_for_read and stack.is_blueprint_setup()) then
+	if not(stack and stack.valid_for_read and stack.is_blueprint) then
 		return
 	end
 
 	local blues = stack.get_blueprint_entities()
-	local ww = false
-	for i,blue in pairs(blues) do
-		if blue.name == "straight-water-way-placed" then
-			blue.name = "straight-water-way"
-			ww=true
-		elseif blue.name == "curved-water-way-placed" then
-			blue.name = "curved-water-way"
-			ww=true
+	if blues ~= nil then
+		local ww = false
+		for i,blue in pairs(blues) do
+			if blue.name == "straight-water-way-placed" then
+				blue.name = "straight-water-way"
+				ww=true
+			elseif blue.name == "curved-water-way-placed" then
+				blue.name = "curved-water-way"
+				ww=true
+			end
 		end
-	end
-	if(ww) then
-		stack.set_blueprint_entities(blues)  
+		if(ww) then
+			stack.set_blueprint_entities(blues)  
+		end
 	end
 end
