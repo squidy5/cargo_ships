55212ec26d6a37c27e17177787d5d9492380ea87
Tom Burrows <tomburrows13@icloud.com>
2023-03-28 21:29:03 +0100

    Refactory CheckRailPlacement

--- logic/rail_placement.lua
+++ logic/rail_placement.lua
@@ -14,60 +14,41 @@ local function get_connected_rails(rail)
 end
 
 function CheckRailPlacement(entity, player, robot)
-  local surface = entity.surface
-  local force = entity.force
-
   if (entity.name == "straight-water-way" or entity.name == "curved-water-way") then
     -- Check if this waterway is connected to a non-waterway
-    local bad_connection = false
-    local bad_name = ""
     if settings.global["prevent_waterway_rail_connections"].value then
       for _, rail in pairs(get_connected_rails(entity)) do
         if not (string.find(rail.name, "%-water%-way") or rail.name == "bridge_crossing") then
-          bad_connection = true
-          bad_name = rail.name
-          break
+          if player then
+            player.print{"cargo-ship-message.error-connect-rails", "__ENTITY__"..entity.name.."__", "__ENTITY__"..rail.name.."__"}
+          else
+            game.print{"cargo-ship-message.error-connect-rails", "__ENTITY__"..entity.name.."__", "__ENTITY__"..rail.name.."__"}
+          end
+          entity.destroy()
+          return
         end
       end
     end
-
-    local name = entity.name
-    if bad_connection then
-      -- Refund ww if connected to rails
-      if player then
-        player.print{"cargo-ship-message.error-connect-rails", "__ENTITY__"..name.."__", "__ENTITY__"..bad_name.."__"}
-      else
-        game.print{"cargo-ship-message.error-connect-rails", "__ENTITY__"..name.."__", "__ENTITY__"..bad_name.."__"}
-      end
-      entity.destroy()
-    else
-      entity.destructible = false
-    end
+    entity.destructible = false
 
   -- Handle non-waterway rail entities
   elseif settings.global["prevent_waterway_rail_connections"].value then
     -- Check if this rail is connected to a waterway
-    local bad_connection = false
-    local bad_name = ""
     for _, rail in pairs(get_connected_rails(entity)) do
       if string.find(rail.name, "%-water%-way") or rail.name == "bridge_crossing" then
-        bad_connection = true
-        bad_name = rail.name
-        break
-      end
-    end
-    if bad_connection then
-      local refund = entity.prototype.items_to_place_this[1]
-      if player then
-        player.insert(refund)
-        player.print{"cargo-ship-message.error-connect-rails", "__ENTITY__"..entity.name.."__", "__ENTITY__"..bad_name.."__"}
-      else
-        if robot then
-          robot.get_inventory(defines.inventory.robot_cargo).insert(refund)
+        local refund = entity.prototype.items_to_place_this[1]
+        if player then
+          player.insert(refund)
+          player.print{"cargo-ship-message.error-connect-rails", "__ENTITY__"..entity.name.."__", "__ENTITY__"..rail.name.."__"}
+        else
+          if robot then
+            robot.get_inventory(defines.inventory.robot_cargo).insert(refund)
+          end
+          game.print{"cargo-ship-message.error-connect-rails", "__ENTITY__"..entity.name.."__", "__ENTITY__"..rail.name.."__"}
         end
-        game.print{"cargo-ship-message.error-connect-rails", "__ENTITY__"..entity.name.."__", "__ENTITY__"..bad_name.."__"}
+        entity.destroy()
+      return
       end
-      entity.destroy()
     end
   end
 end
