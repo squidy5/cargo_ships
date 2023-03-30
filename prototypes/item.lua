1c49ae17e3ba309cd08afcc648863444a627b366
Tom Burrows <tomburrows13@icloud.com>
2023-03-31 00:51:20 +0100

    Remove unnecessary hidden items Removing the bridge items caused pipette to fail, so I removed the scripting that managed it and add placeable_by

--- prototypes/item.lua
+++ prototypes/item.lua
@@ -168,17 +168,6 @@ data:extend{
     place_result = "ship_pump",
     stack_size = 100
   },
-  {
-    type = "item",
-    name = "pump_marker",
-    icon = "__base__/graphics/icons/crude-oil-resource.png",
-    icon_size = 64,
-    icon_mipmaps = 4,
-    flags = {"hidden"},
-    order = "b[fluids]-c[oil_rig]",
-    place_result = "pump_marker",
-    stack_size = 5,
-  },
 }
 
 if settings.startup["deep_oil"].value then
@@ -193,56 +182,6 @@ if settings.startup["deep_oil"].value then
       order = "b[fluids]-c[oil_rig]",
       place_result = "oil_rig",
       stack_size = 5,
-    },
-    {
-      type = "item",
-      name = "or_power",
-      icons = {
-        {icon = GRAPHICSPATH .. "icons/oil_rig.png", icon_size = 64, scale = 0.5},
-        {icon = "__base__/graphics/icons/steam-engine.png", icon_size = 64, icon_mipmaps = 4, scale = 0.25, shift = {0.25, 0.25}}
-      },
-      subgroup = "extraction-machine",
-      flags = {"hidden"},
-      order = "b[fluids]-c[oil_rig]",
-      place_result = "or_power",
-      stack_size = 1,
-    },
-    {
-      type = "item",
-      name = "or_pole",
-      icons = {
-        {icon = GRAPHICSPATH .. "icons/oil_rig.png", icon_size = 64, scale = 0.5},
-        {icon = "__base__/graphics/icons/small-electric-pole.png", icon_size = 64, icon_mipmaps = 4, scale = 0.25, shift = {0.25, 0.25}}
-      },
-      subgroup = "extraction-machine",
-      flags = {"hidden"},
-      order = "b[fluids]-c[oil_rig]",
-      place_result = "or_pole",
-      stack_size = 1,
-    },
-    --[[{
-      type = "item",
-      name = "or_lamp",
-      icon = no_icon,
-      icon_size = 2,
-      subgroup = "extraction-machine",
-      flags = {"hidden"},
-      order = "b[fluids]-c[oil_rig]",
-      place_result = "or_lamp",
-      stack_size = 100,
-    },]]
-    {
-      type = "item",
-      name = "or_radar",
-      icons = {
-        {icon = GRAPHICSPATH .. "icons/oil_rig.png", icon_size = 64, scale = 0.5},
-        {icon = "__base__/graphics/icons/radar.png", icon_size = 64, icon_mipmaps = 4, scale = 0.25, shift = {0.25, 0.25}}
-      },
-      subgroup = "extraction-machine",
-      flags = {"hidden"},
-      order = "b[fluids]-c[oil_rig]",
-      place_result = "or_radar",
-      stack_size = 1,
     },
   }
 end
